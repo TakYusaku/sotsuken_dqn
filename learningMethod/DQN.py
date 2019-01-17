f016868f1dcbc93f9daab02ca12e5383482d82a4
TakYusaku <h26i23@ed.cc.suzuka-ct.ac.jp>
2019-01-17 19:01:35 +0900

    01171901

--- learningMethod/DQN.py
+++ learningMethod/DQN.py
@@ -199,7 +199,7 @@ class DQN:
                 next_action1 = np.argmax(retmainQs1)  # 最大の報酬を返す行動を選択する
                 target = reward_b[0] + gamma * targetQN.model.predict(next_state_b[0])[0][next_action1]
 
-            if not (next_state_b[1] == np.zeros(np.array(state_b[1]).shape)).all(axis=1): # next_state が 0 でない
+            if not (next_state_b[1] == np.zeros(np.array(state_b[1]).shape)).all(): # next_state が 0 でない
                 # 行動決定のQネットワークと価値観数のQネットワークは分離
                 retmainQs2 = self.final.predict(next_state_b[1])[0] # next state に対するpredict
                 next_action2 = np.argmax(retmainQs2)  # 最大の報酬を返す行動を選択する
